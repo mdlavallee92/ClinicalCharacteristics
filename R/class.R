b7793011e92b87e948b3cf8f2ebdd4c980d7eee1
Lavallee,Dr.,Martin (MED MA) BIP-US-R <martin.lavallee@boehringer-ingelheim.com>
2024-04-04 16:31:54 -0400

    add labels and update classes

--- R/class.R
+++ R/class.R
@@ -5,9 +5,11 @@
 
 setClass("breaksStrategy",
          slots = c(
+           name = "character",
            breaks = "data.frame"
          ),
          prototype = list(
+           name = NA_character_,
            breaks = data.frame(
              value = c(),
              grp = c()
@@ -19,11 +21,13 @@ setClass("scoreStrategy",
          slots = c(
            name = "character",
            domain = "character",
+           sql = "character",
            weights = "data.frame"
          ),
          prototype = list(
            name = NA_character_,
            domain = NA_character_,
+           sql = NA_character_,
            weights = data.frame(
              id = c(),
              w = c()
@@ -54,7 +58,8 @@ setClass("executionSettings",
            workDatabaseSchema = "character",
            cohortTable = "character",
            timeWindowTable = "character",
-           codesetTable = "character"
+           codesetTable = "character",
+           dataTable = "character"
          ),
          prototype = list(
            dbms = NA_character_,
@@ -63,25 +68,26 @@ setClass("executionSettings",
            workDatabaseSchema = NA_character_,
            cohortTable = NA_character_,
            timeWindowTable = "#tw",
-           codesetTable = "#Codesets"
+           codesetTable = "#Codesets",
+           dataTable = "#dat"
          )
 )
 
 ## Stow Settings ----------------------------
-setClass("stowSettings",
-         slots = c(
-           dataTable = "character",
-           dataLoc = "character",
-           countLoc = "character",
-           format = "character"
-         ),
-         prototype = list(
-           dataTable = "#dat",
-           dataLoc = fs::file_temp(pattern = "cov", ext = "parquet"),
-           countLoc = fs::file_temp(pattern = "count", ext = "parquet"),
-           format = "parquet"
-         )
-)
+# setClass("stowSettings",
+#          slots = c(
+#            dataTable = "character",
+#            dataLoc = "character",
+#            countLoc = "character",
+#            format = "character"
+#          ),
+#          prototype = list(
+#            dataTable = "#dat",
+#            dataLoc = fs::file_temp(pattern = "cov", ext = "parquet"),
+#            countLoc = fs::file_temp(pattern = "count", ext = "parquet"),
+#            format = "parquet"
+#          )
+# )
 
 ## Age Char --------------------
 
@@ -98,6 +104,7 @@ setClass("ageChar",
          )
 )
 
+## year char ---------------------
 setClass("yearChar",
          slots = c(
            domain = "character",
@@ -1093,4 +1100,490 @@ setMethod("drop_temp_tables", "countChar", function(x){
 
 })
 
+# Get Labels------------------
 
+setGeneric("get_labels", function(x)  standardGeneric("get_labels"))
+
+## ageChar ----------------
+setMethod("get_labels", "ageChar", function(x){
+
+  # get base ids
+  lbl_tbl <- tibble::tibble(
+    category_id = x@orderId,
+    category_name = x@domain,
+    value_id = -999,
+    value_name = x@domain,
+    time_name = "Static from Index"
+  )
+
+  # check if categorized
+  if (!is.null(x@categorize)) {
+    # get value names for  breaks
+    lbl_tbl_cat <- x@categorize@breaks |>
+      dplyr::select(grp_id, grp) |>
+      dplyr::distinct() |>
+      dplyr::rename(
+        value_id = grp_id,
+        value_name = grp
+      ) |>
+      dplyr::mutate(# add category name for breaks
+        category_id = (x@orderId * 1000) + 1,
+        category_name = x@categorize@name,
+        .before = 1
+      ) |>
+      dplyr::mutate(
+        time_name = "Static from Index"
+      )
+    #bind with base table
+    lbl_tbl <- dplyr::bind_rows(
+      lbl_tbl, lbl_tbl_cat
+    )
+  }
+
+  return(lbl_tbl)
+
+})
+
+
+## yearChar ----------------
+setMethod("get_labels", "yearChar", function(x){
+
+  # get base ids
+  lbl_tbl <- tibble::tibble(
+    category_id = x@orderId,
+    category_name = x@domain,
+    value_id = -999,
+    value_name = x@domain,
+    time_name = "Static from Index"
+  )
+
+  # check if categorized
+  if (!is.null(x@categorize)) {
+    # get value names for  breaks
+    lbl_tbl <- x@categorize@breaks |>
+      dplyr::select(grp_id, grp) |>
+      dplyr::distinct() |>
+      dplyr::rename(
+        value_id = grp_id,
+        value_name = grp
+      ) |>
+      dplyr::mutate(# add category name for breaks
+        category_id = (x@orderId * 1000) + 1,
+        category_name = x@categorize@name,
+        .before = 1
+      ) |>
+      dplyr::mutate(
+        time_name = "Static from Index"
+      )
+  } else{
+    # get value names for  breaks
+    lbl_tbl <- year10yrGrp()@breaks |>
+      dplyr::select(value) |>
+      dplyr::mutate(
+        value_id = as.character(value),
+        value_name = glue::glue("Y{value}")
+      ) |>
+      dplyr::mutate(# add category name for breaks
+        category_id = x@orderId,
+        category_name = x@domain,
+        .before = 1
+      ) |>
+      dplyr::mutate(
+        time_name = "Static from Index"
+      ) |>
+      dplyr::select(-c(value))
+  }
+
+  lbl_tbl <- lbl_tbl |>
+    dplyr::mutate(
+      value_name = as.character(value_name)
+    )
+
+  return(lbl_tbl)
+
+})
+
+## demoConceptChar ----------------
+race_categories <- function() {
+  tibble::tribble(
+    ~value_id, ~value_name,
+    8527, "White",
+    38003599, "African American",
+    8516, "Black or African American",
+    8515, "Asian",
+    0, "Not Identified"
+  )
+}
+
+gender_categories <- function() {
+  tibble::tribble(
+    ~value_id, ~value_name,
+    8532, "Female",
+    8507, "Male",
+    0, "Not Identified"
+  )
+}
+
+ethnicity_categories <- function() {
+  tibble::tribble(
+    ~value_id, ~value_name,
+    38003563, "Hispanic or Latino",
+    38003564, "Not Hispanic or Latino",
+    0, "Not Identified"
+  )
+}
+
+
+setMethod("get_labels", "demoConceptChar", function(x){
+
+  if (x@domain == "gender") {
+    lbl_tbl <- gender_categories()
+  }
+
+  if (x@domain == "race") {
+    lbl_tbl <- race_categories()
+  }
+
+  if (x@domain == "ethnicity") {
+    lbl_tbl <- ethnicity_categories()
+  }
+
+  # get base ids
+  lbl_tbl <- lbl_tbl |>
+    dplyr::mutate(
+    category_id = x@orderId,
+    category_name = x@domain,
+    .before = 1
+    ) |>
+    dplyr::mutate(
+      time_name = "Static from Index"
+      )
+
+  return(lbl_tbl)
+
+})
+
+
+## presenceChar ----------------
+setMethod("get_labels", "presenceChar", function(x){
+  time_tbl <- x@time |>
+    dplyr::mutate(
+      time_name = glue::glue("{time_a}d:{time_b}d")
+    ) |>
+    dplyr::select(
+      -c(time_a, time_b)
+    )
+  # get base ids
+  lbl_tbl <- tibble::tibble(
+    value_id = x@tempTables$codeset,
+    value_name = purrr::map_chr(x@conceptSets, ~.x@Name)
+  ) |>
+    tidyr::expand_grid(time_tbl) |>
+    dplyr::mutate(
+      category_id = x@orderId,
+      category_name = glue::glue("presence_{x@domain}"),
+      .before = 1
+    )
+
+  # check if categorized
+  if (!is.null(x@score)) {
+    # get value names for  breaks
+    lbl_tbl_scr <- tibble::tibble(
+      category_id = (x@orderId * 1000) + 1,
+      category_name = glue::glue("presence_{x@domain}"),
+      value_id = -999,
+      value_name = x@score@name
+    ) |>
+      tidyr::expand_grid(time_tbl)
+    #bind with base table
+    lbl_tbl <- dplyr::bind_rows(
+      lbl_tbl, lbl_tbl_scr
+    )
+  }
+
+  return(lbl_tbl)
+})
+
+
+## countChar ----------------
+setMethod("get_labels", "countChar", function(x){
+  time_tbl <- x@time |>
+    dplyr::mutate(
+      time_name = glue::glue("{time_a}d:{time_b}d")
+    ) |>
+    dplyr::select(
+      -c(time_a, time_b)
+    )
+
+  if (!is.null(x@conceptSets)) {
+    # get base ids
+    lbl_tbl <- tibble::tibble(
+      value_id = x@tempTables$codeset,
+      value_name = purrr::map_chr(x@conceptSets, ~.x@Name)
+    )
+  } else {
+    lbl_tbl <- tibble::tibble(
+      value_id = -999,
+      value_name = glue::glue("count_{x@domain}")
+    )
+  }
+
+  # get base ids
+  lbl_tbl <- lbl_tbl |>
+    tidyr::expand_grid(time_tbl) |>
+    dplyr::mutate(
+      category_id = x@orderId,
+      category_name = glue::glue("count_{x@domain}"),
+      .before = 1
+    )
+
+  # check if categorized
+  if (!is.null(x@categorize)) {
+    # get value names for  breaks
+    lbl_tbl_cat <- x@categorize@breaks |>
+      dplyr::select(grp_id, grp) |>
+      dplyr::distinct() |>
+      dplyr::rename(
+        value_id = grp_id,
+        value_name = grp
+      ) |>
+      dplyr::mutate(# add category name for breaks
+        category_id = (x@orderId * 1000) + 1,
+        category_name = glue::glue("count_{x@categorize@name}"),
+        .before = 1
+      ) |>
+      tidyr::expand_grid(time_tbl)
+
+    #bind with base table
+    lbl_tbl <- dplyr::bind_rows(
+      lbl_tbl, lbl_tbl_cat
+    )
+  }
+
+  return(lbl_tbl)
+})
+
+
+
+## costChar ----------------
+setMethod("get_labels", "costChar", function(x){
+  time_tbl <- x@time |>
+    dplyr::mutate(
+      time_name = glue::glue("{time_a}d:{time_b}d")
+    ) |>
+    dplyr::select(
+      -c(time_a, time_b)
+    )
+
+  if (!is.null(x@conceptSets)) {
+    # get base ids
+    lbl_tbl <- tibble::tibble(
+      value_id = x@tempTables$codeset,
+      value_name = purrr::map_chr(x@conceptSets, ~.x@Name)
+    )
+  } else {
+    lbl_tbl <- tibble::tibble(
+      value_id = -999,
+      value_name = glue::glue("cost_{x@domain}")
+    )
+  }
+
+  # get base ids
+  lbl_tbl <- lbl_tbl |>
+    tidyr::expand_grid(time_tbl) |>
+    dplyr::mutate(
+      category_id = x@orderId,
+      category_name = glue::glue("cost_{x@domain}"),
+      .before = 1
+    )
+
+  # check if categorized
+  if (!is.null(x@categorize)) {
+    # get value names for  breaks
+    lbl_tbl_cat <- x@categorize@breaks |>
+      dplyr::select(grp_id, grp) |>
+      dplyr::distinct() |>
+      dplyr::rename(
+        value_id = grp_id,
+        value_name = grp
+      ) |>
+      dplyr::mutate(# add category name for breaks
+        category_id = (x@orderId * 1000) + 1,
+        category_name = glue::glue("cost_{x@categorize@name}"),
+        .before = 1
+      ) |>
+      tidyr::expand_grid(time_tbl)
+
+    #bind with base table
+    lbl_tbl <- dplyr::bind_rows(
+      lbl_tbl, lbl_tbl_cat
+    )
+  }
+
+  return(lbl_tbl)
+})
+
+## timeInChar ----------------
+setMethod("get_labels", "timeInChar", function(x){
+  time_tbl <- x@time |>
+    dplyr::mutate(
+      time_name = glue::glue("{time_a}d:{time_b}d")
+    ) |>
+    dplyr::select(
+      -c(time_a, time_b)
+    )
+
+  lbl_tbl <- tibble::tibble(
+    value_id = -999,
+    value_name = glue::glue("timeIn_{x@domain}")
+  )
+
+  # get base ids
+  lbl_tbl <- lbl_tbl |>
+    tidyr::expand_grid(time_tbl) |>
+    dplyr::mutate(
+      category_id = x@orderId,
+      category_name = glue::glue("timeIn_{x@domain}"),
+      .before = 1
+    )
+
+  # check if categorized
+  if (!is.null(x@categorize)) {
+    # get value names for  breaks
+    lbl_tbl_cat <- x@categorize@breaks |>
+      dplyr::select(grp_id, grp) |>
+      dplyr::distinct() |>
+      dplyr::rename(
+        value_id = grp_id,
+        value_name = grp
+      ) |>
+      dplyr::mutate(# add category name for breaks
+        category_id = (x@orderId * 1000) + 1,
+        category_name = glue::glue("timeIn_{x@categorize@name}"),
+        .before = 1
+      ) |>
+      tidyr::expand_grid(time_tbl)
+
+    #bind with base table
+    lbl_tbl <- dplyr::bind_rows(
+      lbl_tbl, lbl_tbl_cat
+    )
+  }
+
+  return(lbl_tbl)
+})
+
+
+## timeToChar ----------------
+setMethod("get_labels", "timeToChar", function(x){
+  time_tbl <- x@time |>
+    dplyr::mutate(
+      time_name = glue::glue("{time_a}d:{time_b}d")
+    ) |>
+    dplyr::select(
+      -c(time_a, time_b)
+    )
+
+  if (!is.null(x@conceptSets)) {
+    # get base ids
+    lbl_tbl <- tibble::tibble(
+      value_id = x@tempTables$codeset,
+      value_name = purrr::map_chr(x@conceptSets, ~.x@Name)
+    )
+  } else {
+    lbl_tbl <- tibble::tibble(
+      value_id = -999,
+      value_name = glue::glue("timeTo_{x@domain}")
+    )
+  }
+
+  # get base ids
+  lbl_tbl <- lbl_tbl |>
+    tidyr::expand_grid(time_tbl) |>
+    dplyr::mutate(
+      category_id = x@orderId,
+      category_name = glue::glue("timeTo_{x@domain}"),
+      .before = 1
+    )
+
+  # check if categorized
+  if (!is.null(x@categorize)) {
+    # get value names for  breaks
+    lbl_tbl_cat <- x@categorize@breaks |>
+      dplyr::select(grp_id, grp) |>
+      dplyr::distinct() |>
+      dplyr::rename(
+        value_id = grp_id,
+        value_name = grp
+      ) |>
+      dplyr::mutate(# add category name for breaks
+        category_id = (x@orderId * 1000) + 1,
+        category_name = glue::glue("timeTo_{x@categorize@name}"),
+        .before = 1
+      ) |>
+      tidyr::expand_grid(time_tbl)
+
+    #bind with base table
+    lbl_tbl <- dplyr::bind_rows(
+      lbl_tbl, lbl_tbl_cat
+    )
+  }
+
+  return(lbl_tbl)
+})
+
+
+
+## labChar ----------------
+setMethod("get_labels", "labChar", function(x){
+  time_tbl <- x@time |>
+    dplyr::mutate(
+      time_name = glue::glue("{time_a}d:{time_b}d")
+    ) |>
+    dplyr::select(
+      -c(time_a, time_b)
+    )
+
+  labKey <- x@labUnitTable@key |>
+    dplyr::mutate(
+      lab_unit_code = as.numeric(
+        (measurement_concept_id * 1000000) + (unit_concept_id - (floor(unit_concept_id/1000) * 1000))
+      )
+    ) |>
+    dplyr::mutate(
+      unit_name2 = dplyr::if_else(unit_concept_id == 0, "no units", unit_name),
+      value_name = glue::glue("{measurement_name} ({unit_name2})"),
+      value_id = lab_unit_code
+    ) |>
+    dplyr::select(value_id, value_name)
+
+  # get base ids
+  lbl_tbl <- labKey |>
+    tidyr::expand_grid(time_tbl) |>
+    dplyr::mutate(
+      category_id = x@orderId,
+      category_name = glue::glue("Lab Measurements"),
+      .before = 1
+    )
+
+  return(lbl_tbl)
+})
+
+
+## locationChar ----------------
+setMethod("get_labels", "locationChar", function(x){
+
+
+  # get base ids
+  lbl_tbl <- x@locationTable@key |>
+    dplyr::mutate(
+      time_name = "Static from Index"
+    ) |>
+    dplyr::mutate(
+      category_id = x@orderId,
+      category_name = glue::glue("Location"),
+      .before = 1
+    )
+
+  return(lbl_tbl)
+})
