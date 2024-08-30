6bf8caa9eabd024239a3651f96897fa99d00bbe4
Lavallee,Dr.,Martin (MED MA) BIP-US-R <martin.lavallee@boehringer-ingelheim.com>
2024-08-30 13:11:02 -0400

    finalize generate and preview

--- R/ui.R
+++ R/ui.R
@@ -28,21 +28,29 @@ generateTableShell <- function(tableShell, executionSettings, buildOptions = NUL
 
   # Step 5: Aggregate Results Table
   # step 5a: aggregate categorical results first
-  categoricalResultsRaw <- tableShell$aggregateTableShell(executionSettings, type = "categorical")
-  #categoricalResultsFormatted <- .formatCategoricalResult(categoricalResultsRaw)
+  cli::cat_bullet(
+    glue::glue_col("{yellow Aggregating and formatting results}"),
+    bullet = "pointer",
+    bullet_col = "yellow"
+  )
+  categoricalResults <- tableShell$aggregateTableShell(executionSettings, buildOptions, type = "categorical")
+  #categoricalResultsFormatted <- tableShell$.labelResults(categoricalResultsRaw, type = "categorical")
 
   #step 5b: aggregate continuous results second
-  continuousResultsRaw <- tableShell$aggregateTableShell(executionSettings, type = "continuous")
-  #continuousResultsFormatted <- .formatCategoricalResult(continuousResultsRaw)
+  continuousResults <- tableShell$aggregateTableShell(executionSettings, buildOptions, type = "continuous")
+  #continuousResultsFormatted <- tableShell$.labelResults(continuousResultsRaw, type = "continuous")
 
   # keep dat Temp table for future use
-  if (buildOptions$keepDatTable) {
+  if (buildOptions$keepResultsTable) {
     # TODO create ctas to save temp table
+  } else{
+    #close on exit
+    executionSettings$disconnect()
   }
 
   tableShellResults <- list(
-    'categorical' = categoricalResultsFormatted,
-    'continuous' = continuousResultsFormatted
+    'categorical' = categoricalResults,
+    'continuous' = continuousResults
   )
 
   return(tableShellResults)
@@ -88,6 +96,85 @@ saveTableShell <- function(result, outputPath) {
 }
 
 # creates reactable output of the tables shells
-previewTableShell <- function(results) {
+previewTableShell <- function(results, type) {
 
+  if (type == "categorical") {
+    cat_dat <- results$categorical
+
+    res_tb <- reactable::reactable(
+      data = cat_dat,
+      columns = list(
+        'cohortId' = reactable::colDef(name = "Cohort Id"),
+        'cohortName' = reactable::colDef(name = "Cohort Name"),
+        'categoryId' = reactable::colDef(name = "Category Id"),
+        'categoryLabel' = reactable::colDef(name = "Category Name"),
+        'timeId' = reactable::colDef(name = "Time Id"),
+        'twLabel' = reactable::colDef(name = "Time Name"),
+        'valueId' = reactable::colDef(name = "Value Id"),
+        'name' = reactable::colDef(name = "Value Name"),
+        'n' = reactable::colDef(
+          name = "n", format = reactable::colFormat(separators = TRUE)
+        ),
+        'pct' = reactable::colDef(
+          name = "pct", format = reactable::colFormat(percent = TRUE, digits = 2)
+        )
+      ),
+      highlight = TRUE,
+      bordered = TRUE,
+      outlined = TRUE,
+      resizable = TRUE,
+      filterable = TRUE,
+      searchable = TRUE
+    )
+  }
+
+  if (type == "continuous") {
+
+    cts_dat <- results$continuous
+
+    res_tb <- reactable::reactable(
+      data = cts_dat,
+      columns = list(
+        'cohortId' = reactable::colDef(name = "Cohort Id"),
+        'cohortName' = reactable::colDef(name = "Cohort Name"),
+        'categoryId' = reactable::colDef(name = "Category Id"),
+        'categoryLabel' = reactable::colDef(name = "Category Name"),
+        'timeId' = reactable::colDef(name = "Time Id"),
+        'twLabel' = reactable::colDef(name = "Time Name"),
+        'valueId' = reactable::colDef(name = "Value Id"),
+        'n' = reactable::colDef(
+          name = "n", format = reactable::colFormat(separators = TRUE)
+        ),
+        'mean' = reactable::colDef(
+          name = "mean", format = reactable::colFormat(separators = TRUE, digits = 2)
+        ),
+        'sd' = reactable::colDef(
+          name = "sd", format = reactable::colFormat(separators = TRUE, digits = 2)
+        ),
+        'min' = reactable::colDef(
+          name = "min", format = reactable::colFormat(separators = TRUE)
+        ),
+        'p25' = reactable::colDef(
+          name = "25th", format = reactable::colFormat(separators = TRUE, digits = 0)
+        ),
+        'median' = reactable::colDef(
+          name = "median", format = reactable::colFormat(separators = TRUE, digits = 0)
+        ),
+        'p75' = reactable::colDef(
+          name = "75th", format = reactable::colFormat(separators = TRUE, digits = 0)
+        ),
+        'max' = reactable::colDef(
+          name = "max", format = reactable::colFormat(separators = TRUE)
+        )
+      ),
+      highlight = TRUE,
+      bordered = TRUE,
+      outlined = TRUE,
+      resizable = TRUE,
+      filterable = TRUE,
+      searchable = TRUE
+    )
+  }
+
+  return(res_tb)
 }
